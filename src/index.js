019aad9283a16ec3b10c4ae9e35b7ae31605d418
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-01 12:05:36 -0400

    adjusted index.html for testing deployment

--- src/index.js
+++ src/index.js
@@ -14,26 +14,26 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import AuthLayout from "layouts/Auth.js";
-import AdminLayout from "layouts/Admin.js";
+import AuthLayout from 'layouts/Auth.js';
+import AdminLayout from 'layouts/Admin.js';
 
-import "bootstrap/dist/css/bootstrap.css";
-import "assets/scss/paper-dashboard.scss?v=1.3.0";
-import "assets/demo/demo.css";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/scss/paper-dashboard.scss?v=1.3.0';
+import 'assets/demo/demo.css';
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <div>Test</div>
-    <Switch>
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Redirect to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+  // <BrowserRouter>
+  <div>Test</div>,
+  //   <Switch>
+  //     <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+  //     <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+  //     <Redirect to="/admin/dashboard" />
+  //   </Switch>
+  // </BrowserRouter>,
+  document.getElementById('root')
 );
