86085951a2430a469f27e3e9a7752e0515624139
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-09 09:47:31 -0400

    updateing for gpt

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-/* global chrome */
-
 import React, { useEffect, useState } from 'react';
+import './css/App.css';
 
-const App = () => {
-  const [conversations, setConversations] = useState([]);
+function App() {
+  const [conversationNames, setConversationNames] = useState([]);
 
   useEffect(() => {
-    // this is where we would normally fetch the data from the server
-    // but since we are in a content script, we will send a message to it instead
+    // This script will be injected into the active tab and will execute there, reading the DOM.
+    const scriptContent = `
+      const conversationElements = document.querySelectorAll('.flex-1.text-ellipsis.max-h-5.overflow-hidden.break-all.relative');
+      const conversationNames = Array.from(conversationElements).map(element => element.textContent);
+      conversationNames;
+    `;
+
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
-      chrome.tabs.sendMessage(
+
+      chrome.tabs.executeScript(
         activeTab.id,
-        { message: 'get_conversations' },
-        (response) => {
-          setConversations(response);
+        { code: scriptContent },
+        (results) => {
+          // The result of the script is the last expression evaluated, in this case 'conversationNames'.
+          setConversationNames(results[0]);
         }
       );
     });
-  }, []); // empty array means this effect runs once on mount and not on updates
+  }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {conversations.map((conversation, index) => (
-        <button
-          key={index}
-          onClick={() =>
-            chrome.tabs.sendMessage(activeTab.id, {
-              message: 'activate_conversation',
-              position: conversation.position,
-            })
-          }
-        >
-          {conversation.name}
-        </button>
-      ))}
+    <div className="App">
+      <h1>Conversation Names:</h1>
+      <ul>
+        {conversationNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default App;
